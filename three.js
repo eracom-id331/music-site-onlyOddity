6986967ac1c17db64d144acaf89505ff448f3e38
onlyOddity <timote.zin@gmail.com>
2024-10-11 10:35:36 +0200

    section contacts

--- three.js
+++ three.js
@@ -1,20 +1,7 @@
-<script>
-    const audio = document.getElementById('audio-player');
-    const playPauseBtn = document.getElementById('play-pause-btn');
-    const playIcon = document.getElementById('play-icon');
-
-    playPauseBtn.addEventListener('click', function () {
-        if (audio.paused) {
-            audio.play();
-            playIcon.src = 'Image/pause-icon.png'; // Changer l'icône pour "pause"
-        } else {
-            audio.pause();
-            playIcon.src = 'Image/play-icon.png'; // Changer l'icône pour "play"
-        }
-    });
-
-    // Optionnel : Changer l'icône automatiquement à la fin de l'audio
-    audio.addEventListener('ended', function () {
-        playIcon.src = 'Image/play-icon.png';
-    });
-</script>
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Empêche l'envoi réel du formulaire
+    document.getElementById("confirmation-message").style.display = "block"; // Afficher le message de confirmation
+    this.reset(); // Réinitialiser le formulaire
+  });
