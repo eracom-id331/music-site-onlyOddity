e0ff450f846ed0d8cfc37f4ed44b0c745b93532e
onlyOddity <timote.zin@gmail.com>
2024-10-11 10:37:08 +0200

    supresssion three.js

--- three.js
+++
@@ -1,7 +0,0 @@
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Empêche l'envoi réel du formulaire
-    document.getElementById("confirmation-message").style.display = "block"; // Afficher le message de confirmation
-    this.reset(); // Réinitialiser le formulaire
-  });
